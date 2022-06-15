b339eb032dd1a7b2ddb7dad0af13915929779c95
Rimma Iakhina <lenorium>
2022-06-15 12:54:47 +0300

    переписыан метод определения первого игрока, добавлен вывод статуса игры

--- dominoes.py
+++ dominoes.py
@@ -3,6 +3,7 @@ import random
 MIN_WEIGHT = 0
 MAX_WEIGHT = 6
 PIECES_PER_PLAYER = 7
+snake = []
 
 
 def generate_domino_set():
@@ -22,22 +23,40 @@ def draw(stock):
     return reserved
 
 
-def get_start_piece(player1, player2):
+# To start the game, players determine the starting piece.
+# The player with the highest double ([6,6] or [5,5] for example)
+# will donate that domino as a starting piece for the game.
+# After doing so, their opponent will start the game by going first.
+def start_game(computer, human):
     max_double = [MAX_WEIGHT, MAX_WEIGHT]
 
-    if max_double in player1 or max_double in player2:
-        return max_double
+    if max_double in computer:
+        go(computer, max_double)
+        return human
 
-    player1 = find_double_dominoes(player1)
-    player2 = find_double_dominoes(player2)
+    if max_double in human:
+        go(human, max_double)
+        return computer
 
-    if len(player1) == 0:
-        return max(player2)
-    elif len(player2) == 0:
-        return max(player1)
+    c_doubles = find_double_dominoes(computer)
+    h_doubles = find_double_dominoes(human)
+
+    if len(c_doubles) == 0:
+        go(human, max(human))
+        return computer
+
+    if len(h_doubles) == 0:
+        go(computer, max(computer))
+        return human
+
+    max_computer_double = max(c_doubles)
+    max_human_double = max(h_doubles)
+    if max_computer_double > max_human_double:
+        go(computer, max_computer_double)
+        return human
     else:
-        return max(max(player1), max(player2))
-    
+        go(human, max_human_double)
+
 
 def find_double_dominoes(pieces):
     doubles = []
@@ -54,13 +73,28 @@ def has_double_dominoes(domino_set):
     return False
 
 
-def go(player, piece, snake: list):
+def go(player, piece: list):
     snake.append(piece)
     player.remove(piece)
 
 
+def print_status(stock, computer, human, next_player):
+    print('=' * 70)
+    print(f'Stock size: {len(stock)}')
+    print(f'Computer pieces: {len(computer)}\n')
+    print(*snake)
+    print(f'\nYour pieces:')
+    for i, p in enumerate(human):
+        print(f'{i + 1}: {p}')
+
+    if next_player == computer:
+        status = 'Computer is about to make a move. Press Enter to continue...'
+    else:
+        status = "It's your turn to make a move. Enter your command."
+    print(f'\nStatus: {status}')
+
+
 def play():
-    snake = []
     # Domino piece is double if the two numbers written on it are equal
     # If no one has a double domino, the pieces are reshuffled and redistributed.
     while True:
@@ -69,28 +103,8 @@ def play():
         human = draw(stock)
         if has_double_dominoes(computer) or has_double_dominoes(human):
             break
-
-    # To start the game, players determine the starting piece.
-    # The player with the highest double ([6,6] or [5,5] for example)
-    # will donate that domino as a starting piece for the game.
-    # After doing so, their opponent will start the game by going first.
-    # --- ну такое ----
-    init_piece = get_start_piece(computer, human)
-    if init_piece in computer:
-        init_player = computer
-        first_player = human
-    else:
-        init_player = human
-        first_player = computer
-    go(init_player, init_piece, snake)
-    #  --------------------------
-
-    print(f'Stock pieces: {stock}')
-    print(f'Computer pieces: {computer}')
-    print(f'Player pieces: {human}')
-    print(f'Domino snake: {snake}')
-    # TODO исправить потом '!='
-    print(f'Status: {"computer" if first_player == computer else "player"}')
+    next_player = start_game(computer, human)
+    print_status(stock, computer, human, next_player)
 
 
 if __name__ == '__main__':
